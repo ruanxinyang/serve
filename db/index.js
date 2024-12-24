c19aed1b418c35ec26103db049f3c23e6dc73b44
ruanxy <1908167366@qq.com>
2024-12-24 10:24:34 +0800

    加入数据库

--- db/index.js
+++ db/index.js
@@ -1,9 +1,11 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+
 const db = mysql.createPool({
-    host:'rm-wz9s7m9p2c9787v2xvo.mysql.rds.aliyuncs.com',
+    host: 'rm-wz9s7m9p2c9787v2xvo.mysql.rds.aliyuncs.com',
     port: 3306,
     user: 'Ruan1234',
-    password:'ABCabc123!',
-    database:'dev'
-})
-module.exports = db
+    password: 'ABCabc123!',
+    database: 'dev'
+});
+
+module.exports = db;
--- router_handler/sql.js
+++ router_handler/sql.js
@@ -5,7 +5,7 @@ const db = require('../db/index');
 const jwt = require('jsonwebtoken')
 const config = require('../config')
 const uuid = require('uuid');
-const checkToken = (token, callback,res) => {
+const checkToken = (token, callback, res) => {
     if (!token) {
         return res.send({ status: 404, message: '暂无权限' })
     }
@@ -25,34 +25,20 @@ module.exports.sqlList = (req, res) => {
     //获取客户端提供的用户数据
     const data = req.body
     const select = () => {
-    const sql = `select * from action_sql where s_is_del = 1 and sql_name LIKE CONCAT('%','${data.sql_name||''}','%') LIMIT ${data.pageSize} OFFSET ${data.page}`
-    const totalSql = `select count(*) as total from action_sql where s_is_del = 1 and sql_name LIKE CONCAT('%','${data.sql_name||''}','%')`
-    
-        db.query(sql,data.sql_name, (err2, results2) => {
+        const sql = `select * from action_sql where s_is_del = 1 and sql_name LIKE CONCAT('%','${data.sql_name || ''}','%') order by s_create_time desc LIMIT ${data.pageSize} OFFSET ${data.page}`
+        const totalSql = `select count(*) as total from action_sql where s_is_del = 1 and sql_name LIKE CONCAT('%','${data.sql_name || ''}','%')`
+
+        db.query(sql, data.sql_name, (err2, results2) => {
             if (err2) {
                 return res.send({ status: 404, message: err2 })
             }
             const list = results2;
-            db.query(totalSql,data.sql_name, (err3, results3) => {
-                if(err3){
+            db.query(totalSql, data.sql_name, (err3, results3) => {
+                if (err3) {
                     return res.send({ status: 404, message: err3 })
                 }
-                return res.send({ status: 200, data: {intList: list, total: results3[0].total} })
+                return res.send({ status: 200, data: { intList: list, total: results3[0].total } })
             })
-        })
-    }
-    checkToken(data.token, select, res)
-}
-module.exports.editSql = (req, res) => {
-    //获取客户端提供的用户数据
-    const data = req.body
-    const select = (results) => {
-    const sql = `UPDATE action_sql SET sql_name = '${data.sql_name}', sql_content = '${data.sql_content}',s_update_by='${results[0].username}' WHERE sql_id = '${data.sql_id}';`
-        db.query(sql, (err2, results2) => {
-            if (err2) {
-                return res.send({ status: 404, data: err2.message })
-            }
-            return res.send({ status: 200, data: results2 })
         })
     }
     checkToken(data.token, select, res)
@@ -62,16 +48,26 @@ module.exports.addSql = (req, res) => {
     const data = req.body
     const select = (results) => {
         const id = uuid.v7()
-        if(!data.sql_name || !data.sql_content){
+        if (!data.sql_name || !data.sql_content) {
             return res.send({ status: 404, data: '参数不能为空' })
         }
-    const sql = `INSERT INTO action_sql (sql_name,sql_id,s_create_by,sql_content) VALUES ('${data.sql_name}','${id}','${results[0].username}','${data.sql_content}');`
-        db.query(sql, (err2, results2) => {
-            if (err2) {
-                return res.send({ status: 404, data: err2.message })
-            }
-            return res.send({ status: 200, data: {sql_id: id, message: '新增成功'} })
-        })
+        if(data.sql_id){
+            const sql2 = `UPDATE action_sql SET sql_name = '${data.sql_name}', sql_content = '${data.sql_content}',s_update_by='${results[0].username}' WHERE sql_id = '${data.sql_id}';`
+            db.query(sql2, (err2, results2) => {
+                if (err2) {
+                    return res.send({ status: 404, data: err2.message })
+                }
+                return res.send({ status: 200, data: results2 })
+            })
+        }else{
+            const sql = `INSERT INTO action_sql (sql_name,sql_id,s_create_by,sql_content) VALUES ('${data.sql_name}','${id}','${results[0].username}','${data.sql_content}');`
+            db.query(sql, (err2, results2) => {
+                if (err2) {
+                    return res.send({ status: 404, data: err2 })
+                }
+                return res.send({ status: 200, data: { sql_id: id } })
+            })
+        }
     }
     checkToken(data.token, select, res)
 }
@@ -79,7 +75,7 @@ module.exports.deleteSql = (req, res) => {
     //获取客户端提供的用户数据
     const data = req.body
     const select = (results) => {
-    const sql = `UPDATE action_sql SET s_is_del = 0, s_update_by='${results[0].username}' WHERE sql_id = '${data.sql_id}';`
+        const sql = `UPDATE action_sql SET s_is_del = 0, s_update_by='${results[0].username}' WHERE sql_id = '${data.sql_id}';`
         db.query(sql, (err2, results2) => {
             if (err2) {
                 return res.send({ status: 404, message: err2.message })
@@ -88,4 +84,76 @@ module.exports.deleteSql = (req, res) => {
         })
     }
     checkToken(data.token, select, res)
+}
+module.exports.tables = (req, res) => {
+    const data = req.body;
+    // 获取所有表
+    const callback = () => {
+        db.query('SHOW TABLES', (err, tables) => {
+            if (err) throw err;
+
+            const tablePromises = tables.map((table) => {
+                const tableName = table['Tables_in_dev'];
+                return new Promise((resolve) => {
+                    // 获取每个表的字段及备注
+                    db.query(`SHOW FULL COLUMNS FROM ${tableName}`, (err, fields) => {
+                        if (err) throw err;
+
+                        // 提取字段信息
+                        const fieldDetails = fields.map(field => ({
+                            value: field.Field,
+                            label: field.Comment || field.Field, // 如果没有备注，则显示'无备注'
+                            type: field.Type
+                        }));
+                        resolve({ table: tableName, fields: fieldDetails }); // 修改为包含字段详情
+                    });
+                });
+            });
+
+            Promise.all(tablePromises).then((results) => {
+                return res.send({ status: 200, data: results })
+            }).catch(err => {
+                return res.send({ status: 404, data: err })
+            });
+        });
+    };
+
+    checkToken(data.token, callback, res);
+}
+module.exports.fetchTable = (req, res) => {
+    const data = req.body;    
+    if (!data.tableName || !data.params) {
+        return res.send({ status: 404, data: '参数不合法' })
+    }
+    // 获取所有表
+    const callback = async () => {
+        const { params } = data;
+        const newArr = params.map((item)=>{
+            if(item && item.fieldValue && item.fieldName){
+                return `${item.fieldName} = '${item.fieldValue}'`
+            }
+        }).join(' AND ');
+        let sql = `select * from ${data.tableName} LIMIT ${data.pageSize} OFFSET ${data.page}`
+        if(params && params.length > 0 && newArr && newArr.length > 0){
+            sql = `select * from ${data.tableName} where ${newArr} LIMIT ${data.pageSize} OFFSET ${data.page}`
+        }
+        let totalSql = `select count(*) as total from ${data.tableName}`
+        if(params && params.length > 0 && newArr && newArr.length > 0){
+            totalSql = `select count(*) as total from ${data.tableName} where ${newArr}`
+        }
+        db.query(sql, data.sql_name, (err2, results2) => {
+            if (err2) {
+                return res.send({ status: 404, data: err2 })
+            }
+            const list = results2;
+            db.query(totalSql, data.sql_name, (err3, results3) => {
+                if (err3) {
+                    return res.send({ status: 404, data: err3 })
+                }
+                return res.send({ status: 200, data: { intList: list, total: results3[0].total } })
+            })
+        })
+    };
+
+    checkToken(data.token, callback, res);
 }
